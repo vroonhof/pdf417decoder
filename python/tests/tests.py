510a50fa71cf43dbbe30e01cf3759b801ca58313
Jan Vroonhof <jan.vroonhof@gmail.com>
2025-03-25 00:10:32 +0100

    Add a test for decoding and reassembling a MacroPDF encoded file.
    To source non-proprietary input samples and to ensure interoperability
    use images provided by
    https://www.bcgen.com/pdf417-barcode-creator.html
    
    As a thank you leave the attribution in the files.

--- python/tests/tests.py
+++ python/tests/tests.py
@@ -76,6 +76,28 @@ def test_byte_mode_data():
     assert barcode_count == 1
     assert decoder.barcode_binary_data == bytearray(b"\x05\x01\xff\xff\x00\x00062S;Gp\x00\xf2\xed\x10\x00\x00\x14\x1e\x00VR3\x01Y3\x01\x00\x00\x00\x00\x00\x00\x00\x04\x00\x02\x00\x00\\\xda\x00\x008034\xaeiW\rYB\x1c\xd4\x0b\x00\xf2\xd3\x7fO\xf8\xefiS\xa0\xaa\xfb\x9b\xcf0\x16\x13\xc3\x08>\x86Jz\xe8L\xfe\x1f\xebM,R\x05\x00o3\x01\x00")
 
+def test_macropdf():
+    # given an image that has two parts of a multi-bar code datum
+    image = PIL.open("tests/macropdf_part1and2.png")
+    # and another image that the thirst part (e.g. 2nd page_
+    image2 = PIL.open("tests/macropdf_part3.png")
+
+    info_list = []
+    # when we decode first page
+    decoder = PDF417Decoder(image)
+    barcode_count = decoder.decode()
+    assert barcode_count == 2
+    info_list.extend(decoder.barcodes_info)
+
+    # and the 2nd page
+    decoder = PDF417Decoder(image2)
+    barcode_count = decoder.decode()
+    assert barcode_count == 1
+    info_list.extend(decoder.barcodes_info)
+
+    assert len(info_list) == 3
+    assert PDF417Decoder.assemble_data(info_list) == b'One Two Three'
+
 def test_multiple_barcodes():
     # given an image that has multiple barcodes
     image = PIL.open("tests/multiple_barcodes.png")
